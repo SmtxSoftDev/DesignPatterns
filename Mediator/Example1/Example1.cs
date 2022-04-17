58cce46edcbde8f73d2ad5dfb3dbc102e4cccebf
Абсатар уулу Мыктыбек <smtx.muktubek@gmail.com>
2022-04-17 23:30:59 +0600

    Version 1.0 Added class diagrams and images for pattern mediator

--- Mediator/Example1/Example1.cs
+++ Mediator/Example1/Example1.cs
@@ -60,9 +60,9 @@
 
     public class ManagerMediator : Mediator
     {
-        public Employee programmer { get; set; }
-        public Employee tester { get; set; }
-        public Employee customer { get; set; }
+        public ProgrammerEmployee programmer { get; set; }
+        public TesterEmployee tester { get; set; }
+        public CustomerEmployee customer { get; set; }
 
         public override void Send(string msg, Employee employee)
         {
@@ -80,9 +80,9 @@
         public static void Test()
         {
             ManagerMediator manager = new();
-            Employee customer = new CustomerEmployee(manager);
-            Employee programmer = new ProgrammerEmployee(manager);
-            Employee tester = new TesterEmployee(manager);
+            CustomerEmployee customer = new CustomerEmployee(manager);
+            ProgrammerEmployee programmer = new ProgrammerEmployee(manager);
+            TesterEmployee tester = new TesterEmployee(manager);
             manager.customer = customer;
             manager.programmer = programmer;
             manager.tester = tester;
